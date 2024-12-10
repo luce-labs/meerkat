b89cff86599b3c3b39ae14b219d4f75f3045b709
AmoabaKelvin <kel.amoaba@gmail.com>
2024-12-10 22:05:51 +0000

    remove unused `ctx` definition

--- src/server/api/routers/room/room.procedure.ts
+++ src/server/api/routers/room/room.procedure.ts
@@ -5,7 +5,7 @@ import * as services from "./room.service";
 export const roomRouter = createTRPCRouter({
   create: publicProcedure
     .input(inputs.createRoomInput)
-    .mutation(async ({ ctx, input }) => {
+    .mutation(async ({ input }) => {
       return services.createRoom(input);
     }),
 });
