81a42593b1ddd5330b2c12bf2eb9c54c5c93ddb6
kekeli <kekelidompeh@gmail.com>
2024-12-20 18:31:24 +0000

    fix path

--- src/lib/generic-name-generator/index.ts
+++ src/lib/generic-name-generator/index.ts
@@ -3,13 +3,13 @@ import { promises as fs } from "fs";
 export async function generateName(): Promise<string> {
   const adjectives = JSON.parse(
     await fs.readFile(
-      process.cwd() + "/src/lib/room-name-generator/adjectives.json",
+      process.cwd() + "/src/lib/generic-name-generator/adjectives.json",
       "utf-8",
     ),
   ) as string[];
   const nouns = JSON.parse(
     await fs.readFile(
-      process.cwd() + "/src/lib/room-name-generator/names.json",
+      process.cwd() + "/src/lib/generic-name-generator/names.json",
       "utf-8",
     ),
   ) as string[];
